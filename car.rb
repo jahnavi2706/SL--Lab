b3bff4d3f97f7cbdcb895e4700e381779650929a
jahnavi2706 <85930861+jahnavi2706@users.noreply.github.com>
2023-02-22 15:58:58 +0530

    Add files via upload

---
+++ car.rb
@@ -0,0 +1,14 @@
+class Student
+def initialize(id, branch, name)
+@stu_id = id
+@stu_branch = branch
+@stu_name = name
+puts "ID is: #@stu_id"
+puts "Branch is: #@stu_branch"
+puts "Name is: #@stu_name"
+puts "\n"
+end
+end
+xveh = Student. new("1280", "IT", "Divya")
+yveh = Student. new("1281", "IT", "Jahnavi")
+
