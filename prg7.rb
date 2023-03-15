4d6163da88b0f212947755f4070013b760ea243e
jahnavi2706 <85930861+jahnavi2706@users.noreply.github.com>
2023-03-15 23:09:26 +0530

    Update prg7.rb

--- prg7.rb
+++ prg7.rb
@@ -2,8 +2,8 @@ def makes10(x,y)
     return x == 10|| y == 10 || x + y == 10
 end
 
-print makes20(10, 10),"\n"
-print makes20(40, 10),"\n"
-print makes20(15, 20),"\n"
-print makes20(20, 20),"\n"
-print makes20(20, 10),"\n"
+print makes10(10, 10),"\n"
+print makes10(40, 10),"\n"
+print makes10(15, 20),"\n"
+print makes10(20, 20),"\n"
+print makes10(20, 10),"\n"
